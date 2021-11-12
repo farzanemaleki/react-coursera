001662888a092703a2a1e9638a9ae6f6a6be9e14
farzane.maleki <farzane.maleki.fm@gmail.com>
2021-11-12 03:53:51 +0330

    Fetch Post Comment

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -28,7 +28,7 @@ class CommentForm extends Component{
     }
 
     handleSubmit(values) {
-        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+        this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
     }
 
     render(){
@@ -106,7 +106,7 @@ function RenderDish({dish}) {
     );
 }
 
-function RenderComments({ comments, addComment, dishId}) {
+function RenderComments({comments, postComment, dishId}) {
     if(comments != null){
         return(
             <div className="col-12 col-md-5 m-1">
@@ -121,7 +121,7 @@ function RenderComments({ comments, addComment, dishId}) {
                         );
                     })}
                 </ul>
-                <CommentForm dishId={dishId} addComment={addComment} />
+                <CommentForm dishId={dishId} postComment={postComment} />
             </div>
         );
     }else{
@@ -168,7 +168,7 @@ const DishDetail = (props) =>{
                 <div className="row">
                     <RenderDish dish={props.dish} />
                     <RenderComments comments={props.comments}
-                        addComment={props.addComment}
+                        postComment={props.postComment}
                         dishId={props.dish.id}
                     />
                 </div>
